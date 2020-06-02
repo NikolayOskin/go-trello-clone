534ab61e78c17e943fae27aa2423b732402fffd4
Oskin <nickolay.oskin@gmail.com>
2020-06-02 13:11:36 +0300

    update user model

--- model/user.go
+++ model/user.go
@@ -1,6 +1,9 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-	Email string `json:"email"`
-	Password string `json:"password"`
+	ID       primitive.ObjectID `json:"_id" bson:"_id"`
+	Email    string             `json:"email"`
+	Password string             `json:"password"`
 }
