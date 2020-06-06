74330dab909cc25bfeaf72cddfe01fdba1f60ebe
Oskin <nickolay.oskin@gmail.com>
2020-06-06 20:18:29 +0300

    update board model and repository

--- repository/boards.go
+++ repository/boards.go
@@ -1,27 +1,46 @@
 package repository
 
 import (
-	"github.com/NikolayOskin/go-trello-clone/db"
+	"context"
 	"github.com/NikolayOskin/go-trello-clone/model"
-	"fmt"
+	"github.com/NikolayOskin/go-trello-clone/mongodb"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
 )
 
-type Boards struct {}
+type Boards struct{}
 
-func(u *Boards) FetchByUser(user model.User) []model.Board {
-	rows, _ := db.Conn.Query("SELECT title, user_id FROM boards")
+func (b *Boards) FetchByUser(user model.User) []model.Board {
 	var boards []model.Board
 
-	for rows.Next() {
-		board := model.Board{}
+	collection := mongodb.Client.Database("trello").Collection("boards")
+	filter := bson.D{{"user_id", user.ID.Hex()}}
 
-		err := rows.Scan(&board.Title, &board.UserId)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
+	cursor, err := collection.Find(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		boards = append(boards, board)
+	if err = cursor.All(context.TODO(), &boards); err != nil {
+		log.Fatal(err)
 	}
 
 	return boards
+}
+
+func (b *Boards) GetById(id string) (*model.Board, error) {
+	var board model.Board
+
+	collection := mongodb.Client.Database("trello").Collection("boards")
+
+	objId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := collection.FindOne(context.TODO(), bson.M{"_id": objId}).Decode(&board); err != nil {
+		return nil, err
+	}
+	return &board, nil
 }
