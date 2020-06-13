ba7b424dfe9fcfdb8bd4e69fc1fa0a48a98aa606
Oskin <nickolay.oskin@gmail.com>
2020-06-13 17:21:37 +0300

    update routes

--- app/routes.go
+++ app/routes.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/NikolayOskin/go-trello-clone/controller"
 	mid "github.com/NikolayOskin/go-trello-clone/middleware"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
@@ -14,27 +15,32 @@ func (a *App) InitRouting() {
 	a.Router.Use(middleware.AllowContentType("application/json"))
 
 	a.Router.Route("/auth", func(r chi.Router) {
-		r.Post("/sign-in", controller.SignIn)
-		r.Post("/sign-in", controller.SignUp)
+		ctrl := &controller.AuthController{}
+		r.Post("/sign-in", ctrl.SignIn)
+		r.Post("/sign-up", ctrl.SignUp)
+		r.With(mid.JWTCheck).Get("/verify/{code:[0-9]}", ctrl.VerifyEmail)
 	})
 
-	// routes which require JWT authentication
-	a.Router.Route("/", func(r chi.Router) {
+	a.Router.Route("/users", func(r chi.Router) {
 		r.Use(mid.JWTCheck)
+		ctrl := &controller.UserController{}
+		r.Get("/me", ctrl.GetAuthUser)
+		r.Get("/me/boards", ctrl.GetBoards)
+	})
 
-		a.Router.Route("/users", func(r chi.Router) {
-			r.Get("/me", controller.GetAuthUser)
-		})
+	a.Router.Route("/boards", func(r chi.Router) {
+		r.Use(mid.JWTCheck)
+		ctrl := &controller.BoardController{}
+		r.Post("/", ctrl.Create)
+		r.Get("/{id:[a-z0-9]{24}}", ctrl.GetById)
+		r.With(mid.DecodeBoardObj).Put("/{id:[a-z0-9]{24}}", ctrl.Update)
+	})
 
-		a.Router.Route("/boards", func(r chi.Router) {
-			r.Post("/", controller.AddBoard)
-			r.Get("/", controller.GetBoards)
-		})
-
-		a.Router.Route("/cards", func(r chi.Router) {
-			r.Post("/", controller.AddCard)
-			r.Delete("/", controller.DeleteCard)
-			r.Put("/", controller.UpdateCard)
-		})
+	a.Router.Route("/cards", func(r chi.Router) {
+		r.Use(mid.JWTCheck)
+		ctrl := &controller.CardController{}
+		r.Post("/", ctrl.Create)
+		r.Delete("/", ctrl.Delete)
+		r.Put("/{id:[a-z0-9]{24}}", ctrl.Update)
 	})
 }
