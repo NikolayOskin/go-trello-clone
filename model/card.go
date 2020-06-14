b3dc1ce59e1650d33f18027d215b2c1abbb5bca8
Oskin <nickolay.oskin@gmail.com>
2020-06-15 00:17:11 +0300

    cards validation

--- model/card.go
+++ model/card.go
@@ -1,8 +1,12 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Card struct {
-	Text string `json:"text"`
-	TodoListId uint `json:"todolist_id"`
-	BoardId uint `json:"board_id"`
-	Order uint `json:"order"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Text     string             `json:"text" bson:"text" validate:"required"`
+	ListId   string             `json:"list_id" bson:"list_id" validate:"required"`
+	BoardId  string             `json:"board_id" bson:"board_id" validate:"required"`
+	UserId   string             `json:"user_id" bson:"user_id"` // taken from JWT token
+	Position uint               `json:"pos" bson:"pos" validate:"required"`
 }
