ae7d334b1f796600218e76b9d16ebfa14c296cf6
Oskin <nickolay.oskin@gmail.com>
2020-06-16 01:28:50 +0300

    update user model & repository

--- model/user.go
+++ model/user.go
@@ -4,8 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID               primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Email            string             `json:"email" validate:"required"`
-	Password         string             `json:"password" validate:"required"`
+	Email            string             `json:"email" validate:"required,email"`
+	Password         string             `json:"password" validate:"required,min=6"`
 	Verified         bool               `json:"verified"`
 	VerificationCode int                `bson:"confirm_code,omitempty" json:"confirm_code"`
 }
