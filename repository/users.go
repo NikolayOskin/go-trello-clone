5abfcd122603802d34beb7c7bf6db6f36ea99d9c
Oskin <nickolay.oskin@gmail.com>
2020-06-19 11:47:16 +0300

    add GetById method

--- repository/users.go
+++ repository/users.go
@@ -1,3 +1,25 @@
 package repository
 
+import (
+	"context"
+	"github.com/NikolayOskin/go-trello-clone/model"
+	"github.com/NikolayOskin/go-trello-clone/mongodb"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Users struct{}
+
+func (b *Users) GetById(id string) (*model.User, error) {
+	var user model.User
+	col := mongodb.Client.Database("trello").Collection("users")
+	objId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+	if err := col.FindOne(context.TODO(), bson.M{"_id": objId}).Decode(&user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
