66127544f2c97985e9f8be8aea6c1fdfa0fddf3e
Oskin <nickolay.oskin@gmail.com>
2020-06-29 20:25:37 +0300

    adding indexes to db collections

--- docker/mongodb/init-mongo.js
+++ docker/mongodb/init-mongo.js
@@ -3,3 +3,6 @@ db.auth('root', 'root');
 db = db.getSiblingDB('trello');
 
 db.users.createIndex({ "email": 1 }, {unique: true});
+db.boards.createIndex({ "user_id": 1 });
+db.lists.createIndex({ "board_id": 1 });
+db.cards.createIndex({ "board_id": 1 });
