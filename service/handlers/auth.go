26a68ee45c3203e052550bc179eeeff544c5e380
Oskin <nickolay.oskin@gmail.com>
2020-07-08 02:18:02 +0300

    use context.WithTimeout with mongo queries

--- service/handlers/auth.go
+++ service/handlers/auth.go
@@ -3,23 +3,22 @@ package handlers
 import (
 	"context"
 	"errors"
-	"fmt"
+	pb "github.com/NikolayOskin/go-trello-clone/mailer/src"
 	"github.com/NikolayOskin/go-trello-clone/model"
 	"github.com/NikolayOskin/go-trello-clone/mongodb"
-	v "github.com/NikolayOskin/go-trello-clone/service/validator"
+	mailer "github.com/NikolayOskin/go-trello-clone/service"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
-func Authenticate(reqUser *model.User) error {
-	validate := v.New()
-	if err := validate.Struct(reqUser); err != nil {
-		return err
-	}
+func Authenticate(reqUser *model.User, ctx context.Context) error {
 	var user model.User
 	filter := bson.M{"email": reqUser.Email}
-	if err := mongodb.Users.FindOne(context.TODO(), filter).Decode(&user); err != nil {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	if err := mongodb.Users.FindOne(ctx, filter).Decode(&user); err != nil {
 		return errors.New("invalid credentials")
 	}
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(reqUser.Password)); err != nil {
@@ -30,39 +29,44 @@ func Authenticate(reqUser *model.User) error {
 	return nil
 }
 
-func VerifyEmail(u model.User, code string) error {
+func VerifyEmail(u model.User, code string, ctx context.Context) error {
 	var user model.User
-	if code == "" {
-		return errors.New("code must not be empty")
-	}
-	if err := mongodb.Users.FindOne(context.TODO(), bson.M{"_id": u.ID}).Decode(&user); err != nil {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	if err := mongodb.Users.FindOne(ctx, bson.M{"_id": u.ID}).Decode(&user); err != nil {
 		return err
 	}
 	if user.VerificationCode != code {
 		return errors.New("incorrect verification code")
 	}
 	user.Verified = true
-	if _, err := mongodb.Users.UpdateOne(context.TODO(), bson.M{"_id": u.ID}, bson.M{"$set": user}); err != nil {
+	if _, err := mongodb.Users.UpdateOne(ctx, bson.M{"_id": u.ID}, bson.M{"$set": user}); err != nil {
 		return err
 	}
 	return nil
 }
 
-func ResetPassword(email string) error {
+func ResetPassword(email string, ctx context.Context) error {
 	var u model.User
-	if err := mongodb.Users.FindOne(context.TODO(), bson.M{"email": email}).Decode(&u); err != nil {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	if err := mongodb.Users.FindOne(ctx, bson.M{"email": email}).Decode(&u); err != nil {
 		return errors.New("user with this email does not exist")
 	}
 	u.ResetPasswordCode = primitive.NewObjectID().Hex()
-	_, err := mongodb.Users.UpdateOne(context.TODO(), bson.M{"email": email},
+	_, err := mongodb.Users.UpdateOne(ctx, bson.M{"email": email},
 		bson.D{
 			{"$set", bson.D{{"reset_password_code", u.ResetPasswordCode}}},
 		})
 	if err != nil {
 		return err
 	}
-	if err = SendCode(u.ResetPasswordCode, email); err != nil {
-		fmt.Println(err)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	_, err = mailer.Client().ResetPasswordEmail(ctx, &pb.EmailRequest{Email: email, Code: u.ResetPasswordCode})
+	if err != nil {
+		return err
 	}
 
 	return nil
