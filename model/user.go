3e008d08df66abefe55e0e9d5882365788ee5087
Oskin <nickolay.oskin@gmail.com>
2020-07-13 17:49:01 +0300

    add reset password code

--- model/user.go
+++ model/user.go
@@ -3,9 +3,10 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID               primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Email            string             `json:"email" validate:"required,email"`
-	Password         string             `json:"password" validate:"required,passwrd"`
-	Verified         bool               `json:"verified"`
-	VerificationCode int                `bson:"confirm_code,omitempty" json:"confirm_code"`
+	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Email             string             `json:"email" validate:"required,email"`
+	Password          string             `json:"password" validate:"required,passwrd"`
+	Verified          bool               `json:"verified"`
+	VerificationCode  string             `bson:"confirm_code,omitempty" json:"confirm_code"`
+	ResetPasswordCode string             `bson:"reset_password_code,omitempty" json:"reset_password_code"`
 }
