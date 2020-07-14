a5e1c61f32febd38a058f9747c90cfb90cf9ca27
Oskin <nickolay.oskin@gmail.com>
2020-07-14 21:27:46 +0300

    set reset password expiration datetime

--- service/handlers/auth.go
+++ service/handlers/auth.go
@@ -17,8 +17,10 @@ import (
 func Authenticate(reqUser *model.User, ctx context.Context) error {
 	var user model.User
 	filter := bson.M{"email": reqUser.Email}
+
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
+
 	if err := mongodb.Users.FindOne(ctx, filter).Decode(&user); err != nil {
 		return errors.New("invalid credentials")
 	}
@@ -32,32 +34,41 @@ func Authenticate(reqUser *model.User, ctx context.Context) error {
 
 func VerifyEmail(u model.User, code string, ctx context.Context) error {
 	var user model.User
+
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
+
 	if err := mongodb.Users.FindOne(ctx, bson.M{"_id": u.ID}).Decode(&user); err != nil {
 		return err
 	}
 	if user.VerificationCode != code {
 		return errors.New("incorrect verification code")
 	}
-	user.Verified = true
+	user.Verify()
+
 	if _, err := mongodb.Users.UpdateOne(ctx, bson.M{"_id": u.ID}, bson.M{"$set": user}); err != nil {
 		return err
 	}
+
 	return nil
 }
 
 func ResetPassword(email string, ctx context.Context) error {
 	var u model.User
+
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
+
 	if err := mongodb.Users.FindOne(ctx, bson.M{"email": email}).Decode(&u); err != nil {
 		return errors.New("user with this email does not exist")
 	}
 	u.ResetPasswordCode = primitive.NewObjectID().Hex()
 	_, err := mongodb.Users.UpdateOne(ctx, bson.M{"email": email},
 		bson.D{
-			{"$set", bson.D{{"reset_password_code", u.ResetPasswordCode}}},
+			{"$set", bson.D{
+				{"reset_password_code", u.ResetPasswordCode},
+				{"reset_password_expired_at", time.Now().Add(time.Minute * 15)}},
+			},
 		})
 	if err != nil {
 		return err
@@ -66,6 +77,7 @@ func ResetPassword(email string, ctx context.Context) error {
 	if os.Getenv("APP_ENV") != "test" {
 		ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
 		defer cancel()
+
 		_, err = mailer.Client().ResetPasswordEmail(ctx, &pb.EmailRequest{Email: email, Code: u.ResetPasswordCode})
 		if err != nil {
 			return err
@@ -83,10 +95,15 @@ func SetNewPassword(email string, code string, password string) error {
 	if u.ResetPasswordCode != code {
 		return errors.New("invalid credentials")
 	}
+	if time.Now().After(u.ResetPasswordCodeExpiredAt) {
+		return errors.New("code is expired")
+	}
+
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
 	if err != nil {
 		return err
 	}
+
 	_, err = mongodb.Users.UpdateOne(context.TODO(), bson.M{"email": email},
 		bson.D{
 			{"$set", bson.D{{"password", string(hash)}}},
@@ -94,5 +111,6 @@ func SetNewPassword(email string, code string, password string) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
