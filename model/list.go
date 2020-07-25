19d49273f6682be03d8a0828300f48d2cfb83acb
Oskin <nickolay.oskin@gmail.com>
2020-07-26 00:46:44 +0300

    add UpdateList struct

--- model/list.go
+++ model/list.go
@@ -4,9 +4,15 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type List struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Title    string             `json:"title" bson:"title" validate:"required"`
+	Title    string             `json:"title" bson:"title" validate:"required,max=20"`
 	BoardId  string             `json:"board_id" bson:"board_id" validate:"required"`
 	UserId   string             `json:"user_id" bson:"user_id"` // taken from JWT token
 	Position uint               `json:"pos" bson:"pos" validate:"required"`
 	Cards    []Card             `json:"cards"`
+}
+
+type UpdateList struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title  string             `json:"title" bson:"title" validate:"required,max=20"`
+	UserId string             `json:"user_id" bson:"user_id"` // taken from JWT token
 }
