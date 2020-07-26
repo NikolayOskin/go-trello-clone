d766cb502cc5d8600e0882e12ae7c410bae0e5c5
Oskin <nickolay.oskin@gmail.com>
2020-07-27 00:48:16 +0300

    add auth service

---
+++ service/auth/auth.go
@@ -0,0 +1,66 @@
+package auth
+
+import (
+	"crypto/rsa"
+	"errors"
+	"time"
+
+	"github.com/NikolayOskin/go-trello-clone/model"
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Auth struct {
+	privateKey  *rsa.PrivateKey
+	publicKey   *rsa.PublicKey
+	jwtTTLHours time.Duration
+}
+
+func New(privateKey *rsa.PrivateKey, publicKey *rsa.PublicKey, jwtTTLHours time.Duration) (*Auth, error) {
+	if privateKey == nil {
+		return nil, errors.New("PrivateKey cannot be nil")
+	}
+	if publicKey == nil {
+		return nil, errors.New("PublicKey cannot be nil")
+	}
+
+	return &Auth{
+		privateKey,
+		publicKey,
+		jwtTTLHours,
+	}, nil
+}
+
+func (a *Auth) GenerateToken(user model.User) (string, error) {
+	claims := model.JWTClaims{
+		User: model.User{
+			ID:    user.ID,
+			Email: user.Email,
+		},
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(a.jwtTTLHours * time.Hour).Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	tokenStr, err := token.SignedString(a.privateKey)
+
+	return tokenStr, err
+}
+
+func (a *Auth) ValidateToken(tokenStr string) (*model.JWTClaims, error) {
+	claims := model.JWTClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenStr, &claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return a.publicKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := token.Claims.(*model.JWTClaims); ok && !token.Valid {
+		return nil, err
+	}
+	return &claims, nil
+}
